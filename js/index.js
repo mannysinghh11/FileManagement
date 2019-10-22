bf96cd83dda11ba6f96aca948e1ab47e52f3156e
Manny <manpreetdot11@gmail.com>
2019-10-22 01:47:13 -0700

    All Neccessary Files uploaded

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function loginUser(){
+    var config = new AWS.Config({
+        accessKeyId: 'AKIAJESIFCN7Y2XRWZAQ', secretAccessKey: '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', region: 'us-east-1'
+      });
+
+    AWS.config.update(config);
+
+    var username = document.getElementById("emailInput").value;
+    var password = document.getElementById("passwordInput").value;
+
+    var cognitoIdentity = new AWS.CognitoIdentityServiceProvider();
+    var params = {
+      AuthFlow: "USER_PASSWORD_AUTH",
+      AuthParameters: {
+        "USERNAME" : username,
+        "PASSWORD" : password
+      },
+      ClientId: "46u7r0sfehe5s8lgpoht9stavj"
+    }
+
+    cognitoIdentity.initiateAuth(params, function(err, data){
+      if(err){
+        console.log(err);
+        document.getElementById("infoText").innerHTML = err.message;
+        document.getElementById("infoText").className = "form-text alert-warning";
+      }else{
+        localStorage.setItem("AccessToken", data.AuthenticationResult.AccessToken);
+        localStorage.setItem("IdToken", data.AuthenticationResult.IdToken);
+        localStorage.setItem("RefreshToken", data.AuthenticationResult.RefreshToken);
+        window.open("home.html", "_self");
+      }
+    });
+}
---
+++ js/register.js
@@ -0,0 +1,54 @@
+function registerUser(){
+    //First grab all of the information from each of the fields
+    var email = document.getElementById("emailInput").value;
+    var firstName = document.getElementById("firstNameInput").value;
+    var lastName = document.getElementById("lastNameInput").value;
+    var password = document.getElementById("passwordInput").value;
+    var confirmPassword = document.getElementById("confirmPasswordInput").value;
+
+    var config = new AWS.Config({
+        accessKeyId: 'AKIAJESIFCN7Y2XRWZAQ', secretAccessKey: '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', region: 'us-east-1'
+      });
+
+    AWS.config.update(config);
+
+    var cognitoIdentity = new AWS.CognitoIdentityServiceProvider();
+
+    if(password != confirmPassword && password.length >= 6){
+        document.getElementById("infoText").innerHTML = "Please ensure each field is filled out properly.";
+        document.getElementById("infoText").className = "form-text alert-danger";
+    }else{
+        if(email != "" && firstName != "" && lastName != ""){
+            var userInfo = {
+                ClientId: "46u7r0sfehe5s8lgpoht9stavj",
+                Username: email,
+                Password: password,
+                UserAttributes: [{
+                    Name: 'given_name',
+                    Value: firstName
+                },{
+                    Name: 'family_name',
+                    Value: lastName
+                },{
+                    Name: "email",
+                    Value: email
+                }]
+            }
+
+            cognitoIdentity.signUp(userInfo, function(err, data){
+                if(err){
+                    console.log(err);
+                    document.getElementById("infoText").innerHTML = err.message;
+                    document.getElementById("infoText").className = "form-text alert-warning";
+                }else{
+                    console.log(data);
+                    document.getElementById("infoText").innerHTML = "An Email confirmation will be sent to you.";
+                    document.getElementById("infoText").className = "form-text alert-success";
+                }
+            });
+        }else{
+            document.getElementById("infoText").innerHTML = "Please ensure each field is filled out properly.";
+            document.getElementById("infoText").className = "form-text alert-danger";
+        }
+    }
+}
